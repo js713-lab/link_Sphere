6300283965abc916939cd2ce9a706d52e2afaed3
Yip Hong Seng <Js07ink@Gmail.com>
2024-06-27 09:42:38 +0800

    Initial Commit

---
+++ LinkSphere/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LinkSphere
+//
+//  Created by Yip Hong Seng on 27/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LinkSphere/LinkSphereApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkSphereApp.swift
+//  LinkSphere
+//
+//  Created by Yip Hong Seng on 27/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct LinkSphereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
